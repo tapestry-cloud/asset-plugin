c0f033d8f4a8d3629fa091ccb086a3ea817f5e8c
Simon Dann <simon.dann@gmail.com>
2017-05-17 21:09:22 +0100

    :sparkles: added manifest path to constructor

--- src/AssetHelper.php
+++ src/AssetHelper.php
@@ -7,19 +7,42 @@ use League\Plates\Extension\ExtensionInterface;
 
 class AssetHelper implements ExtensionInterface
 {
-    /** @var \Tapestry\Plates\Template */
+    /**
+     * var \Tapestry\Plates\Template
+     */
     public $template;
 
+    /**
+     * @var string
+     */
+    private $manifestPath = '';
+
+    /**
+     * AssetHelper constructor.
+     *
+     * @param $manifestPath
+     */
+    public function __construct($manifestPath)
+    {
+        $this->manifestPath = $manifestPath;
+    }
+
+    /**
+     * @param Engine $engine
+     */
     public function register(Engine $engine)
     {
         $engine->registerFunction('asset', [$this, 'asset']);
     }
 
+    /**
+     * @param $src
+     * @return string
+     */
     public function asset($src)
     {
-        $manifestPath = __DIR__ . '/../rev-manifest.json';
-        if (file_exists($manifestPath)) {
-            $manifest = json_decode(file_get_contents($manifestPath), true);
+        if (file_exists($this->manifestPath)) {
+            $manifest = json_decode(file_get_contents($this->manifestPath), true);
             $filename = pathinfo($src, PATHINFO_BASENAME);
             if (isset($manifest[$filename])) {
                 $src = str_replace($filename, $manifest[$filename], $src);
