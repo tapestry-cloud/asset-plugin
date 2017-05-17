4fc06a2068e4586545149088f1087e4caf2e94d6
Simon Dann <simon.dann@gmail.com>
2017-05-17 21:09:46 +0100

    :sparkles: finished functionality

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -3,6 +3,7 @@
 namespace TapestryCloud\Asset;
 
 use League\Container\ServiceProvider\AbstractServiceProvider;
+use Tapestry\Entities\Configuration;
 use Tapestry\Plates\Engine;
 
 class ServiceProvider extends AbstractServiceProvider
@@ -16,11 +17,21 @@ class ServiceProvider extends AbstractServiceProvider
      * from the ContainerAwareTrait.
      *
      * @return void
+     * @throws \Exception
      */
     public function register()
     {
+        /** @var Engine $engine */
         $engine = $this->getContainer()->get(Engine::class);
-        $helper = new AssetHelper();
+
+        /** @var Configuration $configuration */
+        $configuration = $this->getContainer()->get(Configuration::class);
+
+        if (! $manifestPath = $configuration->get('asset.manifest_path')) {
+            throw new \Exception('You need to set the location of your manifest path in your site configuration.');
+        }
+
+        $helper = new AssetHelper($manifestPath);
         $engine->loadExtension($helper);
     }
 }
