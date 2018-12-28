412f3a4a5e85167b698ed7ab57b996bd77448ba6
Simon Dann <carbontwelve@users.noreply.github.com>
2018-12-28 00:04:27 +0000

    :sparkles: Implemented missing functionality for #4

--- tests/PluginTest.php
+++ tests/PluginTest.php
@@ -7,11 +7,15 @@ use Symfony\Component\Console\Input\ArrayInput;
 use Tapestry\Console\DefaultInputDefinition;
 use Tapestry\Tapestry;
 use TapestryCloud\Asset\AssetHelper;
+use Webmozart\Assert\Assert;
 
 class PluginTest extends \PHPUnit_Framework_TestCase
 {
-    public function testAssetsAreCorrectlyParsed(){
-
+    /**
+     * @throws \Exception
+     */
+    public function testAssetsAreCorrectlyParsed()
+    {
         //
         // The asset helper makes use of the `url` helper function which needs Tapestry loading
         // for it to work. Therefore we init Tapestry with a mock project folder.
@@ -22,13 +26,24 @@ class PluginTest extends \PHPUnit_Framework_TestCase
             '--env' => 'testing'
         ], $definitions));
 
-        $assetPlugin = new AssetHelper(__DIR__ . DIRECTORY_SEPARATOR . 'manifest.json');
+        $assetPlugin = new AssetHelper(__DIR__ . DIRECTORY_SEPARATOR . 'manifests' . DIRECTORY_SEPARATOR . 'manifest_a.json');
 
         $this->assertEquals('http://localhost:3000/js/main.b68bbcd6.js', $assetPlugin->asset('main.js'));
         $this->assertEquals('http://localhost:3000/css/main.b68bbcd6.css', $assetPlugin->asset('main.css'));
 
         $this->assertEquals('http://localhost:3000/js/main.b68bbcd6.js', $assetPlugin->asset('js/main.js'));
         $this->assertEquals('http://localhost:3000/css/main.b68bbcd6.css', $assetPlugin->asset('css/main.css'));
+
+        $this->assertEquals('http://localhost:3000/test/test.css', $assetPlugin->asset('test/test.css'));
+        $this->assertEquals('http://localhost:3000/test.css', $assetPlugin->asset('test.css'));
+
+        $assetPlugin = new AssetHelper(__DIR__ . DIRECTORY_SEPARATOR . 'manifests' . DIRECTORY_SEPARATOR . 'manifest_b.json');
+
+        $this->assertEquals('http://localhost:3000/js/app-0027e65274.js', $assetPlugin->asset('app.js'));
+        $this->assertEquals('http://localhost:3000/style/main-14bb52651e.css', $assetPlugin->asset('main.css'));
+
+        $this->assertEquals('http://localhost:3000/js/app-0027e65274.js', $assetPlugin->asset('js/app.js'));
+        $this->assertEquals('http://localhost:3000/style/main-14bb52651e.css', $assetPlugin->asset('css/main.css'));
 
         $this->assertEquals('http://localhost:3000/test/test.css', $assetPlugin->asset('test/test.css'));
         $this->assertEquals('http://localhost:3000/test.css', $assetPlugin->asset('test.css'));
